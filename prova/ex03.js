c79664eee4ea42b3d0c7bda51979aaf79ae814e5
Lila <lilaar91@gmail.com>
2023-09-27 21:30:36 -0300

    algoritmo ruiim mas é o que tem

--- prova/ex03.js
+++ prova/ex03.js
@@ -8,3 +8,12 @@
 // Para resolução, vou considerar os números repetidos como dezenas, e não em unidades, 
 // senão todos serão repetidos, menos o 5.
 // ou seja, para minha resolução, espero como resposta um array que me devolva "15" e "22";
+
+// 1- primeiro vou transformar num array de string
+
+// 2- depois vou procurar usando o metodo find() e o for loop 
+
+// 2.1.0- outra opção seria usar o filter e ver se retorna mais de 1 elemento.
+
+// 2.1.1- se o tamanho da array que volta do filter for igual a 1 (blablabla.length === 1), adiciona isso na array de
+//numeros solitarios 
