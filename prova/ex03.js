9ec3d192f8164bb0f3aa924718e74398caebc163
Lila <lilaar91@gmail.com>
2023-09-27 21:22:35 -0300

    iniciando a resolucao do ex03

---
+++ prova/ex03.js
@@ -0,0 +1,10 @@
+// Desafio: Número Solitário
+// Neste desafio, seu objetivo é encontrar o(s) número(s) solitário(s) (que não se repete), 
+// a partir do array de números fornecido. Para resolver, crie uma função que recebe um array com os seguintes números:
+
+// [12, 17, 15, 19, 22, 17, 19, 12]
+// E retorne os que não estiverem repetidos.
+
+// Para resolução, vou considerar os números repetidos como dezenas, e não em unidades, 
+// senão todos serão repetidos, menos o 5.
+// ou seja, para minha resolução, espero como resposta um array que me devolva "15" e "22";
